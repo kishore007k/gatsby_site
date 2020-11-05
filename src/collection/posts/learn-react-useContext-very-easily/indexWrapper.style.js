ddb5dbf5ab12486d5e31167bd12bc1d6f96af565
kishore007k <kishorekumarstar07@gmail.com>
2020-11-05 19:23:45 +0530

    Added styling to the blogs single Page
    Signed-off-by: kishore007k <kishorekumarstar07@gmail.com>

---
+++ src/collection/posts/learn-react-useContext-very-easily/indexWrapper.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Wrapper = styled.section`
+  margin-top: 200px;
+  display: block;
+  width: 100%;
+  height: auto;
+
+  img {
+    /* width: 70%; */
+  }
+`
+
+export default Wrapper
